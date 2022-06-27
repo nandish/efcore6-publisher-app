3ea155d1bfc10088671f1a75596edb394ab65cc0
Nandish Desai <NDesai@schools.nyc.gov>
2022-06-27 10:10:38 -0400

    Chapter 2 -Build your First Application using EF Core

---
+++ PublisherData/PubContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PublisherDomain;
+
+namespace PublisherData
+{
+    public class PubContext: DbContext
+    {
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
+
+        // Hardcoding connection string for Demo
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                "Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = PubDatabase"
+            );
+            
+        }
+
+
+    }
+}
--- PublisherDomain/Author.cs
+++ PublisherDomain/Author.cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace PublisherDomain
 {
-    internal class Author
+    public class Author
     {
+        public Author()
+        {
+            Books = new List<Book>();
+        }
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public List<Book> Books { get; set; }
     }
 }
