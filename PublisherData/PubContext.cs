2d414f749f5370b41214e1ef51a62c328fac28a3
Nandish Desai <nandish.desai@rtlsuper.com>
2022-07-13 15:38:26 -0400

    Section 5 - Controlling Database Creation and Schema with Migrations

--- PublisherData/PubContext.cs
+++ PublisherData/PubContext.cs
@@ -3,7 +3,7 @@ using PublisherDomain;
 
 namespace PublisherData
 {
-    public class PubContext: DbContext
+    public class PubContext : DbContext
     {
         public DbSet<Author> Authors { get; set; }
         public DbSet<Book> Books { get; set; }
@@ -14,7 +14,25 @@ namespace PublisherData
             optionsBuilder.UseSqlServer(
                 "Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = PubDatabase"
             );
-            
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Author>().HasData(
+                new Author { Id = 1, FirstName = "Rhoda", LastName = "Lerman" });
+
+
+            var authorList = new Author[]
+            {
+               new Author { Id = 2, FirstName = "Ruth", LastName="Ozeki"}
+                , new Author { Id = 3, FirstName = "Sofia", LastName = "Segovia" }
+                , new Author { Id = 4, FirstName = "Ursula K.", LastName = "LeGuin" }
+                , new Author { Id = 5, FirstName = "Hugh", LastName = "Howey" }
+                , new Author { Id = 6, FirstName = "Isabelle", LastName = "Allende" }
+            };
+
+            modelBuilder.Entity<Author>().HasData(authorList);
         }
 
 
---
+++ ReverseEngineerConsole/PubDatabaseContext.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace ReverseEngineerConsole
+{
+    public partial class PubDatabaseContext : DbContext
+    {
+        public PubDatabaseContext()
+        {
+        }
+
+        public PubDatabaseContext(DbContextOptions<PubDatabaseContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Author> Authors { get; set; } = null!;
+        public virtual DbSet<Book> Books { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = PubDatabase;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Book>(entity =>
+            {
+                entity.HasIndex(e => e.AuthorId, "IX_Books_AuthorId");
+
+                entity.Property(e => e.BasePrice).HasColumnType("decimal(18, 2)");
+
+                entity.HasOne(d => d.Author)
+                    .WithMany(p => p.Books)
+                    .HasForeignKey(d => d.AuthorId);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
